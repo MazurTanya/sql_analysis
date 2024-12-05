73c1661ec7f5537db6ecc3aef0ad838d23042850
MazurTanya <tetyana.zp1@gmail.com>
2024-12-05 15:53:35 +0200

    Update Common attributes for churners.sql

--- Common attributes for churners.sql
+++ Common attributes for churners.sql
@@ -1,10 +1,9 @@
 -- Кількість та частка задоволених/задоволених кліентів
 SELECT
-  COUNT(CASE WHEN Exited = 1 THEN CustomerId END) AS exited,
-  COUNT(CASE WHEN Exited = 0 THEN CustomerId END) AS non_exited,
-  ROUND(COUNT(CASE WHEN Exited = 1 THEN CustomerId END)/ COUNT(*)*100, 2) AS density_exited,
-  ROUND(COUNT(CASE WHEN Exited = 0 THEN CustomerId END  )/ COUNT(*)*100, 2) AS density_non_exited
-FROM
+  COUNT(CASE WHEN Exited = 1 THEN CustomerId END) AS churnedClient,
+  COUNT(CASE WHEN Exited = 0 THEN CustomerId END) AS non_churnedClient,
+  ROUND(COUNT(CASE WHEN Exited = 1 THEN CustomerId END)/ COUNT(*)*100, 2) AS percent_churnedClient
+  FROM
   `bank-churn-441509.bank_churn.churn`;
 
 /* Вплив середньої зарплатні та середнього строку перебування в банку
@@ -17,7 +16,7 @@ FROM
   `bank-churn-441509.bank_churn.churn`
 GROUP BY Exited;
 
-/* Задоволеність клієнтів в залежності від стажу перебування в банку
+/* Втрачений клієнт в залежності від стажу перебування в банку
 SELECT
   COUNT(CustomerId) AS quantity_ExitedCustomers,
   Tenure
@@ -29,13 +28,12 @@ GROUP BY Tenure
 ORDER BY COUNT(CustomerId) DESC;
 
 
-/*Співвідношення незадоволених клієнтів к задоволеним в залежності від того
+/*Співвідношення втрачених клієнтів до загальної кількості в залежності від того
   скільки продуктів банку вони використовували      
 SELECT NumOfProducts,
-  COUNT(CASE WHEN Exited = 1 THEN CustomerId END) AS exited,
-  COUNT(CASE WHEN Exited = 0 THEN CustomerId END) AS non_exited,
-  ROUND(COUNT(CASE WHEN Exited = 0 THEN CustomerId END)
-      / COUNT(CASE WHEN Exited = 1 THEN CustomerId END), 1) AS ratio_non_exitedPerexited
+  COUNT(CASE WHEN Exited = 1 THEN CustomerId END) AS churnedClient,
+  COUNT(CASE WHEN Exited = 0 THEN CustomerId END) AS non_churnedClient,
+  ROUND(COUNT(CASE WHEN Exited = 1 THEN CustomerId END)/ COUNT(CustomerId) * 100, 1)AS percent_churnedClient
 FROM
   `bank-churn-441509.bank_churn.churn`
 GROUP BY NumOfProducts;
